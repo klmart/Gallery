79b6c781d722136b80132b00993b982fbd4f23fe
klmart <kulbaev.mart@gmail.com>
2016-02-18 19:13:34 +0600

    Create contoller methods new and create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_url
+      else render 'new'
+    end
+  end
   def index
+    @users = User.all
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name,:second_name)
   end
 end
